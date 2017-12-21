09a50f537ed353608e66554056033b8acbe76088
Lorenz Coates <lorenz@Lorenzs-MacBook-Pro.local>
2017-12-21 05:07:15 -0500

    For heroku deploy

---
+++ src/config.js
@@ -0,0 +1,27 @@
+export default {
+  apiUrl: 'http://yoursite.com/api/',
+};
+// Fetch GraphCool DB
+//const apiEndpoint = 'https://api.graph.cool/simple/v1/cj8wu1j3a0jg1014149tcyk4l'
+// FBP GraphCool DB
+const apiEndpoint = 'https://api.graph.cool/simple/v1/cjal048xt2yoe0138o971dr6p'
+
+
+const siteConfig = {
+  siteName: 'FETCH',
+  siteIcon: 'ion-flash',
+  footerText: 'FETCH ©2017 Created by Anonymous...',
+};
+const themeConfig = {
+  topbar: 'themedefault',
+  sidebar: 'themedefault',
+  layout: 'themedefault',
+  theme: 'themedefault',
+};
+const language = 'english';
+export {
+  apiEndpoint,
+  siteConfig,
+  language,
+  themeConfig
+};
